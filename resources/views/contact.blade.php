83313047e7bf5b2a14a269988dfbdd7e142a2a9d
VirtualKley <javolkley@gmail.com>
2023-02-05 16:02:14 -0500

    Stored contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-header">Create New Contact</div>
 
                     <div class="card-body">
-                        <form method="POST" action="/contact">
+                        <form method="POST" action="{{ route('contacts.store') }}">
                             @csrf
                             <div class="row mb-3">
                                 <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
